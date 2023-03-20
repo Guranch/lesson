333d96b73729e802522a723acc8147541319ef38
guranch <cit89503@gmail.com>
2023-03-20 19:12:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//1//
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+//2//
+let num='123';
+let sum=Number(num[0])+Number(num[1])+Number(num[2]);
+console.log(sum)
+//3//
+let length=prompt("Довжина:");
+let width=prompt("Ширина:");
+alert(2*(Number(length)+Number(width)));
